3011924020add54f506d559545f566cf212c9f7c
Moonsoon2020 <bogdanvoinov90@yandex.ru>
2022-04-15 10:42:01 +0300

    Initial commit

---
+++ for_DBwork.py
@@ -0,0 +1,155 @@
+from datetime import datetime
+import sqlite3
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class DB:
+    def a(self, *r):
+        logger.info(';'.join([str(i) for i in r]))
+
+    def __init__(self):
+        self.con = sqlite3.connect('db_For_TGbot.sqlite', check_same_thread=False)
+
+    def add_user(self, surname, name, patronymic, post, id_tg):  # добавление пользователя
+        self.a(surname, name,patronymic, post,id_tg)
+        self.con.cursor().execute(f'''INSERT INTO Users(surname, name, patronymic, post, id_tg)
+         VALUES ('{surname}', '{name}', '{patronymic}', {post}, '{id_tg}')''')
+        self.con.commit()
+
+    def add_company(self, title, phone, password):  # добавление компании
+        self.con.cursor().execute(f'''INSERT INTO Companies(title, number_phone, password_cl)
+         VALUES ('{title}', '{phone}', '{password}')''')
+        self.con.commit()
+
+    def delete_company(self, title):  # удаление компании
+        self.con.cursor().execute(f'''DELETE from Companies 
+        WHERE title = \'{title}\'''')
+        self.con.commit()
+
+    def check_mailing(self, text, date, company):  # проверка наличия уведомления
+        if self.con.cursor().execute(f'''SELECT count(*) FROM Mailings
+         WHERE text = '{text}' AND
+          date = '{date}' AND
+           company = \'{company}\'''').fetchall()[0][0] != 0:
+            return True
+        else:
+            return False
+
+    def add_mailing(self, text, date, company):  # добавление уведомления
+        self.con.cursor().execute(f'''INSERT INTO Mailings(text, date, company)
+         VALUES ('{text}', '{date}', '{company}')''')
+        self.con.commit()
+
+    def delete_mailing(self, text, date, company):  # удаление уведомления
+        self.con.cursor().execute(f'''DELETE from Mailings 
+        WHERE text = '{text}' AND
+          date = '{date}' AND
+           company = \'{company}\'''')
+        self.con.commit()
+
+    def check_question_all(self, text_q, text_a, company):  # проверка вопроса на полную уникальность
+        if self.con.cursor().execute(f'''SELECT count(*) FROM Questions
+         WHERE text_q = '{text_q}' AND
+          text_a = '{text_a}' AND
+           company = \'{company}\'''').fetchall()[0][0] != 0:
+            return True
+        else:
+            return False
+
+    def add_question(self, text_q, text_a, company):  # добавление вопроса
+        self.con.cursor().execute(f'''INSERT INTO Questions(text_q, text_a, company)
+                 VALUES ('{text_q}', '{text_a}', '{company}')''')
+        self.con.commit()
+
+    def check_question(self, text_q, company):  # проверка вопроса на существование
+        if self.con.cursor().execute(f'''SELECT count(*) FROM Questions
+         WHERE text_q = '{text_q}' AND
+           company = \'{company}\'''').fetchall()[0][0] != 0:
+            return True
+        else:
+            return False
+
+    def redact_question(self, text_q, text_a, company):  # редактирование вопроса
+        self.con.cursor().execute(f'''UPDATE Questions
+        SET text_a = '{text_a}'
+         WHERE text_q = '{text_q}' AND
+           company = \'{company}\'''')
+        self.con.commit()
+
+    def delete_question(self, text_q, text_a, company):  # удаление вопроса
+        self.con.cursor().execute(f'''DELETE from Questions
+                 WHERE text_q = '{text_q}' AND
+                  text_a = '{text_a}' AND
+                   company = \'{company}\'''')
+        self.con.commit()
+
+    def get_questions(self, company):  # получение всех вопросов для данной компании
+        return '\n'.join([str(x[0] + 1) + '. ' + x[1][0]
+                          for x in enumerate(self.con.cursor().execute(f'''SELECT text_q FROM Questions
+         WHERE company = \'{company}\'''').fetchall())])
+
+    def get_company_password(self, company):  # получение пароля для клиентов компании
+        return self.con.cursor().execute(f'''SELECT password_cl FROM Companies
+         WHERE title = \'{company}\'''').fetchall()[0][0]
+
+    def check_company(self, company):  # проверка компании на существование
+        if self.con.cursor().execute(f'''SELECT count(*) FROM Companies
+         WHERE title = \'{company}\'''').fetchall()[0][0] != 0:
+            return True
+        else:
+            return False
+
+    def remove_user_company(self, id_tg, company):  # редактирование принадлежности пользователя к компании
+        self.con.cursor().execute(f'''UPDATE Users
+                SET company = '{company}'
+                 WHERE id_tg = \'{id_tg}\'''')
+        self.con.commit()
+
+    def check_user_company(self, id_tg):  # проверка принадлежности пользователя к компании
+        if self.con.cursor().execute(f'''SELECT company FROM Users
+         WHERE id_tg = \'{id_tg}\'''').fetchall()[0][0] not in [None, '']:
+            return True
+        else:
+            return False
+
+    def get_answer(self, question, company):  # получение ответа на вопрос
+        return self.con.cursor().execute(f'''SELECT text_a FROM Questions
+         WHERE company = '{company}' AND
+         text_q = \'{question}\'''').fetchall()[0][0]
+
+    def get_ids(self, company):  # получение всех айди пользователей данной компании
+        return [x[0] for x in self.con.cursor().execute(f'''SELECT id_tg FROM Users
+         WHERE company = \'{company}\'''').fetchall()]
+
+    def get_mailings(self):  # получение всех рассылок на сегодня
+        day = datetime.today().strftime('%d.%m.%Y')
+        # print(day)
+        return [(x[0], self.get_ids(x[1])) for x in self.con.cursor().execute(f'''SELECT text, company FROM Mailings
+                WHERE date = \'{day}\'''').fetchall()]
+
+    def get_user_post(self, id_tg):
+        return self.con.cursor().execute(f'''SELECT post FROM Users
+                 WHERE id_tg = \'{id_tg}\'''').fetchall()[0][0]
+
+# bd = DB()
+# bd.add_company('a', 'b', 'c')
+# bd.add_user('a', 'b', 'c', 1, '1234wer')
+# bd.delete_company('a')
+# bd.add_mailing('b', '15.04.2022', 'A')
+# print(bd.check_mailing('a', '01.01.01', 'A'))
+# bd.delete_mailing('a', '01.01.01', 'A')
+# bd.add_question('a3', 'b4', 'Comp')
+# print(bd.check_question_all('a', 'b', 'c'))
+# bd.redact_question('a', 'b', 'c')
+# bd.delete_question('a', 'b', 'c')
+# print(bd.check_question('a', 'c'))
+# print(bd.get_questions('Comp'))
+# print(bd.get_company_password('a'))
+# print(bd.check_company('a'))
+# bd.remove_user_company('1234wer', 'A')
+# print(bd.check_user_company('12432wer'))
+# print(bd.get_answer('a3', 'Comp'))
+# print(bd.get_ids('A'))
+# print(bd.get_mailings())
---
+++ main.py
@@ -0,0 +1,220 @@
+import logging
+from telegram.ext import Updater, MessageHandler, Filters, ConversationHandler
+from telegram.ext import CommandHandler
+from for_DBwork import DB
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+TOKEN = '5342995443:AAEBqyRLrd5AmHEEhCNLyfHVy3td3Qvw-Ec'
+SUPER_PASSWORD = '0000'
+BD = DB()
+
+
+def start(update, context):  #
+    update.message.reply_text('Старт')
+    update.message.reply_text('кто ты')
+    return 1
+
+
+def info(update, context):  # функция уточнения положения
+    a = update.message.text
+    logger.info(' '.join([a, 'Admin', str(a == 'Admin')]))
+    if a == 'Admin':
+        context.user_data['Post'] = 1
+        update.message.reply_text('пароль')
+        return 2
+    else:
+        context.user_data['Post'] = 0
+        update.message.reply_text('ФИО')
+        return 3
+
+
+def stop_reg(update, context):  # функция внезапной остановки
+    update.message.reply_text('остановка')
+    context.user_data.clear()
+    return ConversationHandler.END
+
+
+def password_request(update, context):  # функция проверки суперпароля, используемого для админов
+    a = update.message.text
+    logger.info(' '.join([a, SUPER_PASSWORD, str(a == SUPER_PASSWORD)]))
+    if a == SUPER_PASSWORD:
+        update.message.reply_text('ФИО')
+        return 3
+    else:
+        context.user_data.clear()
+        update.message.reply_text('кто ты')
+        return 1
+
+
+def entering_info(update, context):  # добавление ФИО
+    a = update.message.text
+    context.user_data['ФИО'] = a
+    # добавление пользователя
+    fio = context.user_data['ФИО'].split()
+    logger.info(str(fio) + str(context.user_data['Post']))
+    BD.add_user(fio[0], fio[1], fio[2], context.user_data['Post'], str(update.message.from_user.id))
+    if context.user_data['Post'] == 0:
+        reg_in_company(update, fio[1])
+    context.user_data.clear()
+    return ConversationHandler.END
+
+
+def reg_in_company(update, context):
+    update.message.reply_text('Здравствуйте,  вступиnt в команду. Для этого используйте \\reg_company')
+
+
+def linking_company(update, context):
+    logger.info('fghj')
+    update.message.reply_text('команда')
+    context.user_data['NameCompany'] = update.message.text
+    return 1
+
+
+def get_name_company_password(update, context):
+    a = BD.get_company_password(update.message.text)
+    if a is None:
+        update.message.reply_text('ещё раз')
+        update.message.reply_text('команда')
+        return 1
+    context.user_data['NameCompany'] = update.message.text
+    context.user_data['password'] = a
+    return 2
+
+
+def get_pass(update, context):
+    update.message.reply_text('пароль')
+    if context.user_data['NameCompany'] != update.message.text:
+        update.message.reply_text('ещё раз')
+        update.message.reply_text('команда')
+        return 1
+
+    update.message.from_user.id  # нужный шв
+    return ConversationHandler.END
+
+
+def stop_linking(update, context):
+    update.message.reply_text('Здравствуйте, в команду. Для этого используйте \\reg_company')
+    return ConversationHandler.END
+
+
+def checking_status(update):
+    return False if BD.get_user_post(update.message.from_user.id) == 0 else True
+
+
+def unbinding_company(update, context):
+    # отвязка по конкретному шв
+    pass
+
+
+def get_question(update, context):
+    # отпрвляется шв по нему находится компания, по ней ищется вопрос + проверка компании
+    pass
+
+
+def get_name_company(update, context):
+    if not checking_status(update):
+        update.message.reply_text('у вас нет прав')
+        return ConversationHandler.END
+    update.message.reply_text('имя')
+
+    return 1
+
+
+def password_company(update, context):
+    context.user_data['title'] = update.message.text
+    update.message.reply_text('пароль')
+
+    return 2
+
+
+def get_telephone(update, context):
+    context.user_data['password'] = update.message.text
+    update.message.reply_text('телефон')
+    return 3
+
+
+def creating_company(update, context):
+    BD.add_company(context.user_data['title'], update.message.text, context.user_data['password'])
+    update.message.reply_text('Компания создана')
+    return ConversationHandler.END
+
+
+def stop_new_company(update, context):
+    update.message.reply_text('остановка фвв')
+    return ConversationHandler.END
+
+
+def delete_company(update, context):
+    update.message.reply_text('введите название')
+    BD.delete_company(update.message.text)
+    update.message.reply_text('компания удалена')
+
+
+def helps(update, context):
+    if checking_status(update):
+        update.message.reply_text('помощь админка')
+    else:
+        update.message.reply_text('помощь пользователя')
+
+
+def main():  #
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    script_registration = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('start', start, pass_user_data=True)],
+        # Состояние внутри диалога.
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, info, pass_user_data=True)],
+            2: [MessageHandler(Filters.text & ~Filters.command, password_request, pass_user_data=True)],
+            3: [MessageHandler(Filters.text & ~Filters.command, entering_info, pass_user_data=True)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop_reg, pass_user_data=True)]
+    )
+    dp.add_handler(script_registration)
+    script_linking_company = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('reg_company', linking_company, pass_user_data=True)],
+        # Состояние внутри диалога.
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, get_name_company_password, pass_user_data=True)],
+            2: [MessageHandler(Filters.text & ~Filters.command, get_pass, pass_user_data=True)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop_linking, pass_user_data=True)]
+    )
+    dp.add_handler(script_linking_company)
+    dp.add_handler(CommandHandler("unbinding", unbinding_company))
+    script_creature_company = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('creating_company', get_name_company, pass_user_data=True)],
+        # Состояние внутри диалога.
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, password_company, pass_user_data=True)],
+            2: [MessageHandler(Filters.text & ~Filters.command, get_telephone, pass_user_data=True)],
+            3: [MessageHandler(Filters.text & ~Filters.command, creating_company, pass_user_data=True)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop_new_company, pass_user_data=True)]
+    )
+    dp.add_handler(script_creature_company)
+    dp.add_handler(CommandHandler("delete_company", delete_company))
+    dp.add_handler(CommandHandler("help", helps))
+
+    # самый низ
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, get_question))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
